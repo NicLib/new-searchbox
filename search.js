c38bd447e0e3f08378ef06e63b5c21e620641370
Preston Howell <pshowell23@gmail.com>
2019-07-09 12:27:17 -0400

    Grab queryString

---
+++ search.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function($){
+    //Check JS is loaded
+    console.log("JS Loaded");
+
+    $("#catalog-search").submit(function(e){
+        e.preventDefault();
+        //Check form submits
+        console.log("form submitted");
+
+        let queryString = $(this).children('.input-group').children('#queryString').val();
+
+        //Check queryString is set
+        console.log(queryString);
+    })
+})
