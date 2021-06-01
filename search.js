972ec5edbcdaef3ba2ea424b8946dd97e4d85a1b
Preston Howell <pshowell23@gmail.com>
2021-06-01 10:57:32 -0400

    Update for Discovery v2

--- search.js
+++ search.js
@@ -2,7 +2,12 @@ const BOOKS_AND_MEDIA = '197,239,283,638,1279,1708,1736,1920,1978,2175,2513,3369
 const BOOKS_AND_MEDIA_FORMAT_STRING = "&format=Book&format=Msscr&format=Jrnl&format=News&format=music&format=Video&format=archv&year=all&yearFrom=&yearTo=&language=all";
 const ARTICLES = '638,897,944,1271,1275,1279,1697,1708,1721,1723,1725,1728,1734,1736,1834,1941,1953,1978,2006,2007,2237,2259,2260,2261,2264,2375,2477,2479,2484,3006,3313,3863,3866,3879,3384,4162,10008';
 const ARTICLES_FORMAT_STRING = "&format=Artchap&format=Encyc";
-const DISCOVERY_URL = 'https://anderson.on.worldcat.org/search?sortKey=LIBRARY_PLUS_RELEVANCE';
+const DISCOVERY_URL = 'https://anderson.on.worldcat.org/v2/search?';
+
+
+//https://anderson.on.worldcat.org/v2/search?&databaseList=197,239,283,638,1279,1708,1736,1920,1978,2175,2513,3369,4162,&queryString=hobbit&format=Book&format=Msscr&format=Jrnl&format=News&format=music&format=Video&format=archv&year=all&yearFrom=&yearTo=&language=all
+//https://anderson.on.worldcat.org/v2/search?databaseList=197%2C239%2C283%2C638%2C1279%2C1708%2C1736%2C1920%2C1978%2C2175%2C2513%2C3369%2C4162%2C&queryString=hobbit&format=Book&format=Msscr&format=Jrnl&format=News&format=music&format=Video&format=archv&year=all&yearFrom=&yearTo=&language=all&scope=wz%3A1274&changedFacet=scope&clusterResults=true&groupVariantRecords=false
+
 
 jQuery(document).ready(function($){
     //Check JS is loaded
